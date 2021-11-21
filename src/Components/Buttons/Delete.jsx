eada8b0e24968a18cb5a19e8e9581cf3d8907b09
Killrog <94044562+Killrog@users.noreply.github.com>
2021-11-21 17:05:12 +0100

    Add files via upload

---
+++ src/Components/Buttons/Delete.jsx
@@ -0,0 +1,14 @@
+import { Button } from "grommet";
+
+const Entfernen = () => {
+  return (
+    <Button
+      margin="8px"
+      secondary="true"
+      label="Entfernen"
+      alignSelf="end"
+      size="small"
+    />
+  );
+};
+export default Entfernen;
